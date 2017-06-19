b77ead14de96ec7893a79c718c4c201c3c3d8993
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-06-19 17:09:53 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,18 @@
+
+document.getElementById('')
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください。'));
+
+var message;
+if(answer === number){
+   message = 'あたり！';
+}else if(answer < number){
+   message = '残念！もっと大きいです';
+}else if(answer > number){
+   message = '残念！もっと小さいです';
+}else{
+   message = '0～3の数字を入力してください'}
+   
+   
+   
+ document.getElementById('choice').textComent = message;
