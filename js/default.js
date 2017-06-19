802f86e4683fc767735db1576e0981eb2ba55c72
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-06-19 17:38:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 
-var number = Math.floor(Math.random()*3);
-var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください。'));
+var number = Math.floor(Math.random()*11);
+var answer = parseInt(window.prompt('数あてゲーム。0～10の数字を入力してください。'));
 
 var message;
 if(answer === number){
@@ -10,7 +10,7 @@ if(answer === number){
 }else if(answer > number){
    message = '残念！もっと小さいです';
 }else{
-   message = '0～3の数字を入力してください'}
+   message = '0～10の数字を入力してください'}
    
    
    
